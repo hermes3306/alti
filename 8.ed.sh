055da4cb78ba21df7ca9216c75b0ff4294290920
hermes3306 <joonho.park@hotmail.com>
2022-07-15 07:38:57 +0000

    x

--- 8.ed.sh
+++ 8.ed.sh
@@ -1,1 +1,14 @@
+#ifndef FD_SETSIZE
+#define FD_SETSIZE __FD_SETSIZE
+#endif
+
+  <     __fd_mask fds_bits[__FD_SETSIZE / __NFDBITS];
+  ---
+  >     __fd_mask fds_bits[FD_SETSIZE / __NFDBITS];
+  67c72
+  <     __fd_mask __fds_bits[__FD_SETSIZE / __NFDBITS];
+  ---
+  >     __fd_mask __fds_bits[FD_SETSIZE / __NFDBITS];
+
+
 sudo vi /usr/include/x86_64-linux-gnu/sys/select.h
