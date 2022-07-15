a0c7ff867e66307366cefd153305e2bd337a203a
Joonho Park <pi@DESKTOP-DLJE090.localdomain>
2022-07-15 15:49:12 +0900

    x

--- 5.flex-bison.sh
+++ 5.flex-bison.sh
@@ -2,13 +2,13 @@ sudo yum remove flex bison
 
 tar xvzf flex-2.5.39.tar.gz
 cd flex-2.5.39
-configure; make; sudo make install
+./configure; make; sudo make install
 sudo ln -s /usr/local/bin/flex /usr/bin/flex
 flex --version
 
 cd ..
 tar xvzf bison-2.4.1.tar.gz
 cd bison-2.4.1
-configure; make; sudo make install
+./configure; make; sudo make install
 sudo ln -s /usr/local/bin/bison /usr/bin/bison
 bison --version
