5a724778384429c134d80c7c55c36ca6378eb837
hermes3306 <joonho.park@hotmail.com>
2022-07-26 14:02:57 +0900

    gcc

---
+++ gcc/down.sh
@@ -0,0 +1,7 @@
+wget https://ftp.gnu.org/gnu/gmp/gmp-5.0.5.tar.bz2
+wget https://www.bugseng.com/products/ppl/download/ftp/releases/0.11.2/ppl-0.11.2.tar.bz2
+wget https://www.bugseng.com/products/ppl/download/ftp/releases/1.0/ppl-1.0.tar.bz2
+wget https://ftp.gnu.org/gnu/mpfr/mpfr-3.1.1.tar.bz2
+wget https://ftp.gnu.org/gnu/mpc/mpc-1.0.1.tar.gz
+wget ftp://gcc.gnu.org/pub/gcc/infrastructure/cloog-ppl-0.15.11.tar.gz
+wget ftp://ftp.fu-berlin.de/unix/languages/gcc/releases/gcc-4.6.3/gcc-4.6.3.tar.bz2
