cd14f4029df1f1debe373988e3144ea32e866ffe
hermes3306 <joonho.park@hotmail.com>
2022-07-31 10:10:44 +0000

    altilinker

--- 9.tar.sh
+++ 9.tar.sh
@@ -1,16 +1,16 @@
-#tar xvzf ab71.tgz
-#sed -i 's/ulj/#ulj/' altibase-7.1.0.0.0/src/ul/Makefile
+tar xvzf ab71.tgz
+sed -i 's/ulj/#ulj/' altibase-7.1.0.0.0/src/ul/Makefile
 grep ulj altibase-7.1.0.0.0/src/ul/Makefile
 
-#sed -i 's/adapter/#adapter/' altibase-7.1.0.0.0/ut/Makefile
+sed -i 's/adapter/#adapter/' altibase-7.1.0.0.0/ut/Makefile
 grep adapter altibase-7.1.0.0.0/ut/Makefile
 
-#sed -i 's/altiMon/#altiMon/' altibase-7.1.0.0.0/ut/Makefile
+sed -i 's/altiMon/#altiMon/' altibase-7.1.0.0.0/ut/Makefile
 grep altiMon altibase-7.1.0.0.0/ut/Makefile
 
-#sed -i 's/altilink/#altilink/' altibase-7.1.0.0.0/src/dk/Makefile
-grep altiMon altibase-7.1.0.0.0/src/dk/Makefile
+sed -i 's/altilinker/#altilinker/' altibase-7.1.0.0.0/src/dk/Makefile
+grep altilinker altibase-7.1.0.0.0/src/dk/Makefile
 
-#mv altibase-7.1.0.0.0 ../ab71
+mv altibase-7.1.0.0.0 ../ab71
 cp go.sh ../ab71
 
