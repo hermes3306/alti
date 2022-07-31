43a44043d4f553afbde32ba2b4bab32854831236
hermes3306 <joonho.park@hotmail.com>
2022-07-31 06:26:20 +0000

    x

---
+++ 9.tar.sh
@@ -0,0 +1,13 @@
+#tar xvzf ab71.tgz
+#sed -i 's/ulj/#ulj/' altibase-7.1.0.0.0/src/ul/Makefile
+grep ulj altibase-7.1.0.0.0/src/ul/Makefile
+
+#sed -i 's/adapter/#adapter/' altibase-7.1.0.0.0/ut/Makefile
+grep adapter altibase-7.1.0.0.0/ut/Makefile
+
+#sed -i 's/altiMon/#altiMon/' altibase-7.1.0.0.0/ut/Makefile
+grep altiMon altibase-7.1.0.0.0/ut/Makefile
+
+#mv altibase-7.1.0.0.0 ../ab71
+cp go.sh ../ab71
+
--- go.sh
+++ go.sh
@@ -1,3 +1,5 @@
+# comment out java library src/ul/ulj ut/altimon tools/adaptor etc
+
 ./configure --with-build_mode=release ## default build mode is debug mode
   make clean
   make build
