e41682ae13fe6de27f30f0bae0d03bc84e53d69b
hermes3306 <joonho.park@hotmail.com>
2022-07-31 10:06:16 +0000

    x

--- 9.tar.sh
+++ 9.tar.sh
@@ -8,6 +8,9 @@ grep adapter altibase-7.1.0.0.0/ut/Makefile
 #sed -i 's/altiMon/#altiMon/' altibase-7.1.0.0.0/ut/Makefile
 grep altiMon altibase-7.1.0.0.0/ut/Makefile
 
+#sed -i 's/altilink/#altilink/' altibase-7.1.0.0.0/src/dk/Makefile
+grep altiMon altibase-7.1.0.0.0/src/dk/Makefile
+
 #mv altibase-7.1.0.0.0 ../ab71
 cp go.sh ../ab71
 
