7a322211c16bd20baca23a5fc9cd2953959d3990
hermes3306 <joonho.park@hotmail.com>
2022-07-31 06:53:07 +0000

    x

--- 3.jdk.sh
+++ 3.jdk.sh
@@ -1,6 +1,11 @@
-tar xvzf jdk-15.0.1_linux-x64_bin.tar.gz 
-tar xvzf jdk-17.0.3.1_linux-x64_bin.tar.gz
+#tar xvzf jdk-15.0.1_linux-x64_bin.tar.gz 
+#tar xvzf jdk-17.0.3.1_linux-x64_bin.tar.gz
+#tar xvzf jdk-18_linux-x64_bin.tar.gz
+#ln -s jdk15 jdk-15.0.1/
+#ln -s jdk17 jdk-17.0.3.1/
+#ln -s jdk18 jdk-18.0.1.1/
+
+wget https://download.oracle.com/java/18/latest/jdk-18_linux-x64_bin.tar.gz
 tar xvzf jdk-18_linux-x64_bin.tar.gz
-ln -s jdk15 jdk-15.0.1/
-ln -s jdk17 jdk-17.0.3.1/
-ln -s jdk18 jdk-18.0.1.1/
+mv jdk-18.0.2 ../jdk18
+
